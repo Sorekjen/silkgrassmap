7ca16fbe4218c7f1ae69ed97baa2b5de37ab5c18
Jakob Kjellerup <sorenkjellerup@gmail.com>
2023-07-05 16:49:13 +0200

    initial commit

---
+++ src/utils/farmData.jsx
@@ -0,0 +1,33 @@
+export default farmData = {
+    one: {
+        name: 'one',
+        size: 78,
+        types: [{name: "orange", amount: 23}, {name: "coconut", amount: 153}],
+    },
+    two: {
+        name: 'two',
+        size: 102,
+        types: [{name: "vanilla", amount: 11}, {name: "cacao", amount: 234}],
+    },
+    three: {
+        name: 'three',
+        size: 512,
+        types: [{name: "coconut", amount: 314}],
+    },
+    four: {
+        name: 'four',
+        size: 243,
+        types: [{name: "pineapple", amount: 23}, {name: "mango", amount: 39}],
+    },
+    five: {
+        name: 'five',
+        size: 54,
+        types: [{name: "avacados", amount: 21}, {name: "ginger", amount: 42}],
+    },
+    six: {
+        name: 'six',
+        size: 123,
+        types: [{name: "tumeric", amount: 15}, {name: "legumes", amount: 46}],
+    },
+
+}
